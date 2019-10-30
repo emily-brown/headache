4b640ecf1ed48d596b6af06a34bf12f17cc2e217
Emily Brown <emily_brown@rogers.com>
2019-10-29 21:16:09 -0400

    react build

---
+++ client/build/precache-manifest.e6f047a762921313352822508b59008a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb42da2cad394136889",
+    "url": "/static/css/main.ec0f9bbe.chunk.css"
+  },
+  {
+    "revision": "8cb42da2cad394136889",
+    "url": "/static/js/main.8cb42da2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cb4e9ac88ba150f709c5",
+    "url": "/static/js/2.cb4e9ac8.chunk.js"
+  },
+  {
+    "revision": "cba9066e75deda8131d6c8c5fda13a61",
+    "url": "/index.html"
+  }
+];
